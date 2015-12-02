2e77544ad258c47b22c06196a62b7a15dea40d69
aleen42 <aleen42@vip.qq.com>
2015-12-02 22:24:57 +0800

    comment

--- src/color.js
+++ src/color.js
@@ -1,17 +1,34 @@
 "use strict";
 
 var SColor = {
-
+	/**
+	 * [image: the object of an image]
+	 * @type {[type]}
+	 */
 	image: null,
 
+	/**
+	 * [init: constructor]
+	 * @param  {Image} image [the image]
+	 * @return {[type]}       [description]
+	 */
 	init: function (image) {
 		this.image = image;
 	},
 	
+	/**
+	 * [createCanvas: create a dom of 'canvas']
+	 * @return {[type]} [description]
+	 */
 	createCanvas: function () {
 		return document.createElement('canvas');
 	},
 
+	/**
+	 * [convertRGB: generate a format of rgb]
+	 * @param  {[type]} data [the data return of getImageData]
+	 * @return {[type]}      [description]
+	 */
 	convertRGB: function (data) {
 		return {
 			r: data[0],
@@ -20,6 +37,14 @@ var SColor = {
 		};
 	},
 
+	/**
+	 * [getBlock: get the color of a block of the image]
+	 * @param  {[type]} top    [x]
+	 * @param  {[type]} left   [y]
+	 * @param  {[type]} width  [the width of the block]
+	 * @param  {[type]} height [the height of the block]
+	 * @return {[type]}        [description]
+	 */
 	getBlock: function (top, left, width, height) {
 		"use strict";
 		var canvas = this.createCanvas();
